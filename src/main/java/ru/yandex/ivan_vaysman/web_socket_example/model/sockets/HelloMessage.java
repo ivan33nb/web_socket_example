5952848eb7f0a9d065d13aa85d3065ca8fa7b3fb
out-stroki-waisman <k5LpLsM5Kgjy>
2022-08-23 17:11:20 +0300

    Added models

---
+++ src/main/java/ru/yandex/ivan_vaysman/web_socket_example/model/sockets/HelloMessage.java
@@ -0,0 +1,13 @@
+package ru.yandex.ivan_vaysman.web_socket_example.model.sockets;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class HelloMessage {
+
+    private String name;
+}
