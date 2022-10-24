b2a11cd465d4b6a5d4d1ae81a8ce14ef3e869f88
out-stroki-waisman <k5LpLsM5Kgjy>
2022-10-24 13:30:16 +0300

    Added test controller

---
+++ src/main/java/ru/yandex/ivan_vaysman/web_socket_example/controller/TestController.java
@@ -0,0 +1,9 @@
+package ru.yandex.ivan_vaysman.web_socket_example.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+}
